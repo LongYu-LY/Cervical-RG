0b1f1ee76f89d23a657a31b4f8cfe659767867c0
yu long <61861796+LongYu-LY@users.noreply.github.com>
2026-01-02 16:51:18 +0800

    Update builder.py
    修复bug

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -46,7 +46,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
         kwargs['attn_implementation'] = 'flash_attention_2'
 
     
-    if 'Cervical' in model_name.lower():
+    if 'cervical' in model_name.lower():
         print(f'Initializing with LongLLaVA')
         tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True, trust_remote_code=True)
         # quantization_config = BitsAndBytesConfig(load_in_8bit=True, llm_int8_skip_modules=["mamba"])
@@ -149,7 +149,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
 
     image_processor = None
 
-    if 'llava' in model_name.lower() or 'llava' in model_path.lower():
+    if 'cervical' in model_name.lower() or 'cervical' in model_path.lower():
         mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
         mm_use_im_patch_token = getattr(model.config, "mm_use_im_patch_token", True)
         if mm_use_im_patch_token:
